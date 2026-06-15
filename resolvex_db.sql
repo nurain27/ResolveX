01676413b79f9f22039371e228ee73795bf83e38
khirunnisya <khirunnisyan@gmail.com>
2026-06-16 00:49:52 +0800

    update database

--- resolvex_db.sql
+++ resolvex_db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 13, 2026 at 04:44 PM
+-- Generation Time: Jun 15, 2026 at 06:35 PM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- PHP Version: 8.1.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
